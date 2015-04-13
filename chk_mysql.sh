473d20ae6b08c3679d0339a349f5656aa24180f0
torvalds0310 <yaning@lm-shc-00355625.corp.ebay.com>
2015-04-13 16:49:55 +0800

    Updated comments.

--- chk_mysql.sh
+++ chk_mysql.sh
@@ -12,22 +12,22 @@ serverip=`ifconfig | grep "inet addr" | head -n 1 | awk '{print $2}' | awk -F ":
 currdate=`date +"%y-%m-%d-%H:%M:%S"`
 
 # Check mysql if runing.
-$mysqladmin -u$user -p$passwd -h$host status 1>>/dev/null 2>/home/mysqlstatus.log
+$mysqladmin -u$user -p$passwd -h$host status 1>2 2>/home/mysqlstatus.log
 
 if [ $? -ne 0 ]; then
-	echo "$currdate" > /home/mysqlstatus.log
-	echo "Mysql is not runing, please run command "/etc/init.d/mysqld start" to start it." > /home/mysqlstatus.log
+	echo "$currdate" >> /home/mysqlstatus.log
+	echo "Mysql is not runing on server $serverip at $currdate." > /home/mysqlstatus.log
 	mail -s "Mysql is not running on server $serverip at $currdate." yaning@blabla.com < /home/mysqlstatus.log
 	exit 1
 fi
 
 
 # Check mysql if functionnal.
-$mysql -u$user -p$passwd -h$host -e "insert into test.mytest values(1,'test');" 1>/dev/null 2>/home/mysqlinsert.log
+$mysql -u$user -p$passwd -h$host -e "insert into test.mytest values(1,'test');" 1>2 2>/home/mysqlinsert.log
 
 if [ $? -ne 0 ]; then
 	echo "$currdate" >> /home/mysqlinsert.log
-	echo "MySQL is not functional as can not insert data into table test.mytest."
+	echo "MySQL is not functional on server $serverip at $currdate." >> /home/mysqlinsert.log
 	mail -s "MySQL is not functional on server $serverip at $currdate." yaning@blabla.com < /home/mysqlinsert.log
 fi
 
